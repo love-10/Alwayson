9082a260d47be7f7c1ee883270551fc610e88e90
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-07-25 22:05:15 +0800

    rec

--- app/src/main/java/com/example/alwayson/ItemListView.kt
+++ app/src/main/java/com/example/alwayson/ItemListView.kt
@@ -2,7 +2,9 @@ package com.example.alwayson
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,9 +26,47 @@ class ItemListView(context: Context, attributeSet: AttributeSet) :
     init {
         addView(binding.root)
         binding.rec.apply {
-            layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, true)
             adapter = itemAdapter
+            addItemDecoration(HeaderFooterItemDecoration(500, 500))
+        }
+        binding.rec.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val centerView = findCenterView(recyclerView)
+                centerView?.let {
+                    val position = recyclerView.getChildAdapterPosition(it)
+                    // 在这里处理位于屏幕中间的item
+                    Log.d(
+                        "xxxxx",
+                        "中间的item位置: ${(itemAdapter.getItem(position) as Bean).name}"
+                    )
+                }
+            }
+        })
+    }
+
+    fun findCenterView(recyclerView: RecyclerView): View? {
+        val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return null
+        val childCount = recyclerView.childCount
+        if (childCount == 0) {
+            return null
         }
 
+        val centerPosition = recyclerView.width / 2
+        var centerView: View? = null
+        var minDistance = Int.MAX_VALUE
+
+        for (i in 0 until childCount) {
+            val child = recyclerView.getChildAt(i)
+            val childCenter = (child.right + child.left) / 2
+            val distance = kotlin.math.abs(childCenter - centerPosition)
+
+            if (distance < minDistance) {
+                minDistance = distance
+                centerView = child
+            }
+        }
+        return centerView
     }
 }
