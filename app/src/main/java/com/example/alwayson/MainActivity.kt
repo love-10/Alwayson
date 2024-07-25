32fd07f6e29ef0a4052e4a5e163ee6af177212d5
梦之流风 <71875705+love-10@users.noreply.github.com>
2024-07-25 22:35:35 +0800

    rec

--- app/src/main/java/com/example/alwayson/MainActivity.kt
+++ app/src/main/java/com/example/alwayson/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.alwayson
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.TextView
 import com.example.alwayson.databinding.ActivityMainBinding
 
@@ -17,6 +22,24 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
+        binding.btn.setOnClickListener {
+            showPopupWindow(it)
+        }
+    }
+
+    private fun showPopupWindow(anchorView: View) {
+        // 创建 PopupWindow 的布局视图
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = inflater.inflate(R.layout.pop, null)
+
+        // 创建 PopupWindow
+        val popupWindow = PopupWindow(popupView,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            true)
+
+        // 显示 PopupWindow
+        popupWindow.showAsDropDown(anchorView, 0, -250)
     }
 
     /**
